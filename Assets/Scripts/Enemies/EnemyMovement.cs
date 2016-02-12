dd84f39fa37ca01648dd7045e3f1e72c33156fec
SuperSaiyan4 <joeywelvaadt@joeys-mbp.media.local>
2016-02-12 10:00:18 +0100

    12-02-16
    Simple enemy movement + simple player movement

---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	private Vector3 _walkToPos;
+	private Vector3 _currentPos;
+
+	void Update() {
+		Wander ();
+	}
+
+	void Wander() {
+		if (_currentPos == Vector3.zero || _currentPos == _walkToPos) {
+			_walkToPos = new Vector3 (Random.Range (-36f, 36f), Random.Range (-9f, 9f), 0);
+			Debug.Log ("In if: " + _walkToPos);
+		}
+
+		_currentPos = transform.position;
+
+		transform.position = Vector3.MoveTowards (_currentPos, _walkToPos, 0.05f);
+		Debug.Log ("Out if: " + _walkToPos);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private float _speed = 7f;
+
+	void Update () {
+		Move ();
+	}
+
+	void Move() {
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		transform.Translate (x * _speed * Time.deltaTime, y * _speed * Time.deltaTime,0);
+	}
+}
