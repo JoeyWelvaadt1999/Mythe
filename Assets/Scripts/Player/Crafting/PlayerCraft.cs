6be4e5aa445b262f23ad00997bd8de4d624af247
DenzelXIII <denzel_justin@hotmail.com>
2016-02-12 12:05:22 +0100

    Started working on the basic crafting

---
+++ Assets/Scripts/Player/Crafting/PlayerCraft.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCraft : MonoBehaviour
+{
+    Craftables _craftables = new Craftables();
+
+	// Use this for initialization
+	void Start ()
+    {
+        print("Resources required to make a torch : " + "Wood: "+ _craftables.Torch.wood + " Stones: " + _craftables.Torch.stones);
+        print("Resources required to make a fur coat" + _craftables.FurCoat.fur);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
