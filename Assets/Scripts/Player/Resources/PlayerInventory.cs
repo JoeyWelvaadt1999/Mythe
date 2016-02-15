fd67666b459d8329e43f00628b427de0ab76ee68
SuperSaiyan4 <joeywelvaadt@Joeys-MacBook-Pro.local>
2016-02-15 10:15:55 +0100

    15-02-16 10:15
    Fixed spelling errors

---
+++ Assets/Scripts/Player/Resources/PlayerInventory.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+	private Resources _playerResources = new Resources();
+	public Resources PlayerResources {
+		get { 
+			return _playerResources;
+		}
+	}
+
+	void Start() {
+//		_playerInventory.Fur += 	
+	}
+}
