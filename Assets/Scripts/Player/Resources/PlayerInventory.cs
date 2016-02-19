8a1699cc373eb71f734044cb20aa533312c48a9c
SuperSaiyan4 <joeywelvaadt@Joeys-MacBook-Pro.local>
2016-02-19 16:40:13 +0100

    19-02-16 16:40
    finished playerinventory and resources

--- Assets/Scripts/Player/Resources/PlayerInventory.cs
+++ Assets/Scripts/Player/Resources/PlayerInventory.cs
@@ -2,7 +2,18 @@
 using System.Collections;
 
 public class PlayerInventory : MonoBehaviour {
+	[SerializeField]private Resource _resourceObject; //Set this game object to the gameobject(resource) that is picked up
+	private ResourceTypes.ResourceType _resourceType = new ResourceTypes.ResourceType();
 	private Resources _playerResources = new Resources();
+
+	public Resource ResourceObject {
+		get { 
+			return _resourceObject;
+		} set { 
+			_resourceObject = value;
+		}
+	}
+
 	public Resources PlayerResources {
 		get { 
 			return _playerResources;
@@ -10,6 +21,25 @@ public class PlayerInventory : MonoBehaviour {
 	}
 
 	void Start() {
-        _playerResources.Fur += 4;
+		_resourceType = _resourceObject.ResourceType;
+//        _playerResources.Fur += 4;
+		Debug.Log(_resourceType);
+	}
+
+	void SetState(){
+		switch (_resourceType) {
+		case ResourceTypes.ResourceType._fur:
+			_playerResources.Fur += _resourceObject.Value;
+			break;
+		case ResourceTypes.ResourceType._meat:
+			_playerResources.Meat += _resourceObject.Value;
+			break;
+		case ResourceTypes.ResourceType._stone:
+			_playerResources.Stones += _resourceObject.Value;
+			break;
+		case ResourceTypes.ResourceType._wood:
+			_playerResources.Wood += _resourceObject.Value;
+			break;
+		}
 	}
 }
