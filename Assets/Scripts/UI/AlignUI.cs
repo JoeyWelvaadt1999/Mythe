45a0d5ab9c44d3e1de744ac7dce995a3d9e3b017
SuperSaiyan4 <joeywelvaadt@Joeys-MacBook-Pro.local>
2016-03-09 16:25:10 +0100

    09-03-16 16:25
    Added UI

--- Assets/Scripts/UI/AlignUI.cs
+++ Assets/Scripts/UI/AlignUI.cs
@@ -1,30 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using VectorUtils;
 
 
 public class AlignUI : MonoBehaviour {
 	[SerializeField]private Canvas _canvas;
-	[Range(-100,100)][SerializeField]private int _percentageX;
-	[Range(-100,100)][SerializeField]private int _percentageY;
+	[Range(-100f,100f)][SerializeField]private float _percentageX;
+	[Range(-100f,100f)][SerializeField]private float _percentageY;
+	[Range(1f,2f)][SerializeField]private float _scale;
+	[SerializeField]private float _width;
+	[SerializeField]private float _height;
 	private RectTransform _canvasRect;
-	private RectTransform _rect;
+	private RectTransform _rectTransform;
+	private RectTransform[] _childTransforms;
+	private Rect _rect;
+
+	public float Scale {
+		get { 
+			return _scale;
+		}
+	}
 
 	void Start() {
 		_canvasRect = _canvas.GetComponent<RectTransform> ();
-		_rect = GetComponent<RectTransform> ();
-		_rect.anchoredPosition = CalculatePosition ();
+		_rectTransform = GetComponent<RectTransform> ();
+		_childTransforms = this.GetComponentsInChildren<RectTransform> ();
 	}
 
 	public Vector2 CalculatePosition() {
-		_canvasRect = _canvas.GetComponent<RectTransform> ();
-		_rect = GetComponent<RectTransform> ();
-		float x = 0 + ((_canvasRect.rect.width / 2) / _percentageX - (_rect.rect.width / 2) / _percentageX);
-		float y = 0 + ((_canvasRect.rect.height / 2) / _percentageY - (_rect.rect.height / 2) / _percentageY);
-		_rect.anchoredPosition = new Vector2(x, y);
+		
+		float x = 0 + ((_canvasRect.rect.width / 2) / _percentageX - (_rectTransform.rect.width / 2) / _percentageX);
+		float y = 0 + ((_canvasRect.rect.height / 2) / _percentageY - (_rectTransform.rect.height / 2) / _percentageY);
+		_rectTransform.anchoredPosition = new Vector2(x, y);
 		return new Vector2 (x, y);
 	}
+		
 
+	private void SetWidthHeight() {
+		if (_width * _scale <= _canvasRect.rect.width) {
+			_rect.width = _width * _scale;
+			_rect.height = _height * _scale;
+
+		} else {
+			_scale = _scale;
+		}
+
+		_rectTransform.sizeDelta = new Vector2(_rect.width, _rect.height);
+//		_rectTransform.localScale = new Vector2 (1, 1);
+	}
+		
+	void Update () {
+		SetWidthHeight ();
+		_rectTransform.anchoredPosition = CalculatePosition ();
+	}
 
 	void OnValidate() {
 		if (_percentageX == 0) {
