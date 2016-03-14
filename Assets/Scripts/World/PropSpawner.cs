1ce80d6d049a7866064db3e948d51f1e7e9bb150
BartGeest <kkcbart@live.nl>
2016-03-14 10:54:51 +0100

    14-2-2016 10:55 up to date for check-up

--- Assets/Scripts/World/PropSpawner.cs
+++ Assets/Scripts/World/PropSpawner.cs
@@ -9,13 +9,16 @@ public class PropSpawner : MonoBehaviour
     private GameObject[] _props;
 
     private float _maxProps = 200;
+    private float _maxClusters = 3;
+
+    [SerializeField]
+    private GameObject _treeCluster;
+    private Vector2 _clusterbounds;
+
     private float _minDis = 1; //minimal distance between props/propclusters
     private float _maxDis = 10; //maximal distance ^
+    
 
-    private float _clusterRadius;
-
-
-    // Use this for initialization
     void Start()
     {
         //CreateTreeCluster();
@@ -27,6 +30,7 @@ public class PropSpawner : MonoBehaviour
     {
         for (int i = 0; i < _maxProps; i++)
         {
+
             //new position every loop iteration
             float randomPosX = Random.Range(-_bounds.x, _bounds.x);
             float randomPosY = Random.Range(-_bounds.y, _bounds.y);
@@ -34,11 +38,9 @@ public class PropSpawner : MonoBehaviour
             //random gameobject every iteration
             GameObject randomProp = _props[Random.Range(0, _props.Length)];
 
-            GameObject propClone = (GameObject)Instantiate(randomProp, new Vector3(randomPosX, randomPosY, 0), Quaternion.identity);
+            Instantiate(randomProp, new Vector3(randomPosX, randomPosY, 0), Quaternion.identity);
         }
     }
-    
-    //for advanced propplacement later
     /*
     void CreatePropPosition()
     {
@@ -47,7 +49,10 @@ public class PropSpawner : MonoBehaviour
 
     void CreateTreeCluster()
     {
-        for loop that creates placements for trees within radius
+        for (int i = 0; i < _maxClusters; i++)
+        {
+            
+        }
     }
     */
 }
---
+++ Assets/Scripts/World/TreeCluster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeCluster : MonoBehaviour
+{
+    private Vector2 _clusterBounds;
+    private float _maxTrees;
+
+    private float _minClusterWidth = 5;
+    private float _maxClusterWidth = 15;
+    private float _minClusterHeight = 5;
+    private float _maxClusterHeight = 15;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        CreateRandomClusterBounds();
+    }
+
+    //creates the size of a cluster, maxTrees is based of the size of a cluster
+    void CreateRandomClusterBounds()
+    {
+        _clusterBounds = new Vector2(
+            Random.Range(_minClusterWidth, _maxClusterWidth),
+            Random.Range(_minClusterHeight, _maxClusterHeight)
+            );
+
+        //_maxTrees = _clusterBounds.x * _clusterBounds.y;
+        //Debug.Log(_maxTrees);
+    }
+}
